ed9c9ed4348e8532bd384a4d282b11d313940e18
Ajay Kumar <ajay.kumar2@hexaviewtech.com>
2025-12-27 18:22:16 +0530

    MergeSort

--- src/MergeSort.java
+++ src/MergeSort.java
@@ -3,32 +3,69 @@ import java.util.Arrays;
 public class MergeSort {
 
     public static int[] mergeSort(int[] arr) {
-        int[] temp = new  int[arr.length];
-        mergeSortHelper(arr, 0, arr.length - 1, temp);
+        mergeSortHelper(arr, 0, arr.length - 1);
         return arr;
     }
 
-    private static void mergeSortHelper(int[] arr, int left, int right, int[] temp) {
+    private static void mergeSortHelper(int[] arr, int left, int right) {
         if (left >= right) {
             return;
         }
 
         int mid = left + (right - left)/2;
 
-        mergeSortHelper(arr, left, mid, temp);
-        mergeSortHelper(arr, mid + 1, right, temp);
+        mergeSortHelper(arr, left, mid);
+        mergeSortHelper(arr, mid + 1, right);
 
-        merge(arr, left, mid, right, temp);
+        merge(arr, left, mid, right);
     }
 
-    private static void merge(int[] arr, int left, int mid, int right, int[] temp) {
+    private static void merge(int[] arr, int left, int mid, int right) {
+        int l1 = mid - left + 1;
+        int l2 = right - mid;
 
+        //temp array
+        int[] leftTemp = new int[l1];
+        int[] rightTemp = new int[l2];
+
+        //Copy to temp array
+        for (int i = 0; i < l1; i++) {
+            leftTemp[i] = arr[left + i];
+        }
+        for (int i = 0; i < l2; i++) {
+            rightTemp[i] = arr[mid + 1 + i];
+        }
+
+        //Sort
+        int i = 0, j = 0, k = left;
+        while (i < l1 && j < l2) {
+            if (leftTemp[i] <= rightTemp[j]) {
+                arr[k] = leftTemp[i];
+                i++;
+            } else {
+                arr[k] = rightTemp[j];
+                j++;
+            }
+            k ++;
+        }
+
+        while (i < l1) {
+            arr[k] = leftTemp[i];
+            i++;
+            k++;
+        }
+
+        while (j < l2) {
+            arr[k] = rightTemp[j];
+            j++;
+            k++;
+        }
     }
 
     public static void main(String[] args) {
-        int[] arr = {2,8,4,9,3};
+        int[] arr = {2,8,4,9,3, 214, 11, 88, 78, 88};
 
-        int[] finalArr = mergeSort(arr);;
+        int[] finalArr = mergeSort(arr);
         System.out.println(Arrays.toString(finalArr));
     }
 }
